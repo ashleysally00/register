0b2dc96067ca5bc2ed22d107d2b043ca8f0ec0d1
ashleysally00 <74416365+ashleysally00@users.noreply.github.com>
2024-05-27 17:17:57 -0700

    Added dashboard.js

---
+++ my-app/FrontEnd/src/components/dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LogoutButton from "./logoutButton.js";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Your Dashboard</h1>
+      <LogoutButton />
+    </div>
+  );
+};
+
+export default Dashboard;
